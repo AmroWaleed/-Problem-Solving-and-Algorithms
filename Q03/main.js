bdb49576313e790641a81ad6971270bb8149ac04
AmroWaleed <amro.alkharoof@gmail.com>
2020-11-24 19:28:12 +0200

    solve Q3

--- Q03/main.js
+++ Q03/main.js
@@ -3,11 +3,18 @@ console.log('Problem Solving Q: 3 ');
 /* First Reverse */
 
 /*  
-Have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order. For example: if the input string is "Hello World and Coders" then your program should return the string sredoC dna dlroW olleH.
+Have the function FirstReverse(str) 
+take the str parameter being passed and return the string in reversed order.
+ For example: if the input string is "Hello World and Coders" 
+ then your program should return the string sredoC dna dlroW olleH.
 */
 
 function firstReverse(str) {
-  // YOUR CODE HERE
+  var stringSplite = str.split("");
+  var reversingArray = stringSplite.reverse();
+  var joiningArray = reversingArray.join("");
+  return joiningArray;
+
 }
 
 /* 
