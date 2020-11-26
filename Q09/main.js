37f56a3b0fd3174f30bd0705b79beafa8100ddc1
AmroWaleed <amro.alkharoof@gmail.com>
2020-11-26 15:12:27 +0200

    try to solve Q9

--- Q09/main.js
+++ Q09/main.js
@@ -1,13 +1,26 @@
-console.log('Problem Solving Q: 9');
+console.log("Problem Solving Q: 9");
 
 /* Longest Sequence */
 
 /*  
 Write a function that returns the length of the longest sequence of 1s in an array of 1s and 0s.
 */
-
-function longestSequence() {
-  // YOUR CODE HERE
+/*
+I tryed to solve them 
+I think we should use math.max but there is a wrong here  
+ */
+function longestSequence(array) {
+  let count = 0;
+  let max = 0;
+  for (let index = 0; index < array.length; index++) {
+    if (array[i] === 0) count === 0;
+    else {
+      count++;
+      max = Math.max;
+    }
+  }
+  if (max === 0) return "there is no sequence";
+  else return max;
 }
 
 /* 
