a1269e395ca8ef833213c0653e6db4c467bac5cf
AmroWaleed <amro.alkharoof@gmail.com>
2020-12-27 17:40:20 +0200

    try to solve q4

--- 02_Data Structures + Algorithms [27 Q]/exercises/04_maxchar/index.js
+++ 02_Data Structures + Algorithms [27 Q]/exercises/04_maxchar/index.js
@@ -4,7 +4,10 @@
 // --- Examples
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
-
+/*
+i think we should to count the char of array using an object
+then we have the max char as a string if the char used +1
+*/ 
 function maxChar(str) {}
 
 module.exports = maxChar;
